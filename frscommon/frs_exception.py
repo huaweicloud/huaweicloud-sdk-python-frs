1ed63dfc2a8613e4f8338863a075b69c2acfe456
FrsSvcDeveloper <liangzhiping@huawei.com>
2018-12-18 19:38:28 +0800

    2.0.0 Version. Python PEP8.

--- frscommon/frs_exception.py
+++ frscommon/frs_exception.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 class FrsException(Exception):
-    def frs_exception(self, http_status_code, msg):
-        super(Exception, self).__init__("{\"http_code\": %s,\n\"msg\": \"%s\"}" % (http_status_code, msg))
+    def __init__(self, http_status_code, msg):
+        super(Exception, self).__init__("{\"http_code\": %s, \"msg\": \"%s\"}" % (http_status_code, msg))
